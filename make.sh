00043516579945f7a91cab11f8edc4e6bef7c551
Christian Sutter <git@doublesignal.com>
2023-03-20 15:31:30 +0000

    Tweak make script

--- make.sh
+++ make.sh
@@ -2,19 +2,23 @@
 set -eoux pipefail
 
 # Run `govuk-docker`'s Makefile for all applications for a local experience
+# Time: ~20 minutes from scratch
+
+# Start Elasticsearch before search-api so it has time to come up (race condition)
+govuk-docker up -d elasticsearch-6
 
 # Ensure nginx-proxy is running
 #   (required for my Colima setup so that the services can talk to each other at the expected
 #   domains)
 govuk-docker up -d nginx-proxy
 
-# Set up search applications
-make search-api
-make publishing-api
-make finder-frontend
-
 # Set up publishing applications
+make publishing-api
 make content-publisher
 make content-tagger
 make government-frontend
 make whitehall
+
+# Set up search applications
+make search-api
+make finder-frontend
